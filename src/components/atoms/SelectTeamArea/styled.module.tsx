25d0ceb7c9e9096da8497892d6d6fcd770b41dbd
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-05-16 18:13:54 -0300

    feat: create SelectTeamArea component

---
+++ src/components/atoms/SelectTeamArea/styled.module.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SelectTeamAreaStyled = styled.div`
+  border-bottom: 1px solid #D3D3D3;
+  border-top: 1px solid #D3D3D3;
+  height: 50px;
+  position: fixed;
+  left: 72px;
+  width: calc(100% - 72px);
+  top: 0;
+`;
