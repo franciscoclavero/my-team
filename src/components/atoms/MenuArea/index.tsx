99e29adb78a621f58d7b89c4621e7d728bf420e3
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-05-16 15:23:35 -0300

    feat: create MenuArea component

---
+++ src/components/atoms/MenuArea/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { MenuAreaStyled } from './styled.module';
+
+const MenuArea = () => {
+  return (
+    <MenuAreaStyled>
+      Menu
+    </MenuAreaStyled>
+  );
+};
+
+export default MenuArea;
